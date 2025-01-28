48fde7418b4f8e5a65f289142b2e32b99c049da6
Anton4k12 <neanton2008@gmail.com>
2025-01-28 19:10:58 +0200

    feat: font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        BoldItalic: ["KappaDisplay"],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
